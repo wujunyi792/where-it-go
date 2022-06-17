18222945d4ea4ec64601b896ff49fed906ee9c02
Wu JunYi <wu65830600@163.com>
2022-06-18 00:37:20 +0800

    hide secret message

--- internal/router/v1/router.go
+++ internal/router/v1/router.go
@@ -11,19 +11,19 @@ import (
 func MainRouter(e *gin.Engine) {
 	e.Any("", func(c *gin.Context) {
 		data := struct {
-			UA         string
-			Host       string
-			Method     string
-			Proto      string
-			RemoteAddr string
-			Message    string
+			UA     string
+			Host   string
+			Method string
+			Proto  string
+			//RemoteAddr string
+			Message string
 		}{
-			UA:         c.Request.Header.Get("User-Agent"),
-			Host:       c.Request.Host,
-			Method:     c.Request.Method,
-			Proto:      c.Request.Proto,
-			RemoteAddr: c.Request.RemoteAddr,
-			Message:    fmt.Sprintf("Welcome to %s, version %s.", config.GetConfig().ProgramName, config.GetConfig().VERSION),
+			UA:     c.Request.Header.Get("User-Agent"),
+			Host:   c.Request.Host,
+			Method: c.Request.Method,
+			Proto:  c.Request.Proto,
+			//RemoteAddr: c.Request.RemoteAddr,
+			Message: fmt.Sprintf("Welcome to %s, version %s.", config.GetConfig().ProgramName, config.GetConfig().VERSION),
 		}
 		middleware.Success(c, data)
 	})
